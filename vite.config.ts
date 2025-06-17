0bcbe3f67be075d446352e08bb6c84a80326e668
dev-fuadhasan <hasan2305341284@diu.edu.bd>
2025-06-18 00:38:41 +0600

    fix: Update Vite config for Netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
   server: {
     port: 3000,
   },
